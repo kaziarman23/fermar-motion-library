356ae8797dc749fc09ea9ce5177fc86e40fb7d4c
kaziarman23 <kaziarman@proton.me>
2024-06-14 08:39:50 +0600

    basic of this library is complite

--- src/Components/05.ScrollLinked/ScrollLinked.jsx
+++ src/Components/05.ScrollLinked/ScrollLinked.jsx
@@ -1,23 +1,31 @@
-import { motion, useScroll, useSpring } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import LoremText from "./LoremText";
 
 function ScrollLinked() {
-  const { scrollYProgress } = useScroll();
-  const scaleX = useSpring(scrollYProgress, {
-    stiffness: 100,
-    damping: 30,
-    restDelta: 0.001,
-  });
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress);
+    const background = useTransform(
+        scrollYProgress,
+        [0, 1],  // define the state
+        ["rgb(255, 0, 0)", "rgb(00, 255, 00)"]  //  define the color for state
+    );
 
-  return (
-    <div className="scroll-linked">
-      <motion.div
-        className="progress-bar"
-        style={{ scaleX, backgroundColor: "red" }}
-      />
-      <LoremText />
-    </div>
-  );
+    return (
+        <div className="bg-blue-500">
+            <motion.div
+                style={{
+                    scaleX,
+                    background,
+                    transformOrigin: screenLeft,
+                    position: "sticky",
+                    top: 0,
+                    width: "100%",
+                    height: "10px",
+                }}
+            />
+            <LoremText />
+        </div>
+    );
 }
 
 export default ScrollLinked;
